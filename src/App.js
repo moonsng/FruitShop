8e468d2adeac8719a9138280e699e3d6621f6a7a
Moonseong <ericajg324@naver.com>
2023-08-19 08:39:14 +0900

    Router added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Row, Col} from 'react-bootstrap';
 import { useState } from 'react';
 
 import data from './data.js';
+import {Routes, Route, Link} from 'react-router-dom';
 
 function App() {
 
@@ -12,23 +13,47 @@ function App() {
 
   return (
     <div className="App">
+      
        <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">프루티과수원</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Link to="/">home</Link>
+    <Link to="/detail">detail</Link>
+            {/* <Nav.Link href="#home">Home</Nav.Link> */}
+            {/* <Nav.Link href="#link">Link</Nav.Link> */}
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    <div className='main-bg'></div>
-<div className = "row">
-    <Card fruits = {fruits[0]} i = {1}></Card>
-  <Card fruits = {fruits[1]} i = {2}></Card>
-  <Card fruits = {fruits[2]} i = {3}></Card>
+
+    <Routes>
+      <Route path = "/" element={
+        <>
+         <div className='main-bg'></div>
+         <div className='containter'>
+           <div>{fruits.map((a,i)=>{
+             return <Card fruits={fruits[i]} i={i}></Card>
+           })}
+           </div>
+         </div>
+         </>
+      }/>
+        <Route path = "/detail" element={<div>상세페이지입니다.</div>}/> 
+      </Routes>
     </div>
   );
 }
@@ -36,11 +61,12 @@ function App() {
 function Card(props){
   return (
     <div className="col-md-4">
-      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
-      <h4>{ props.shoes.title }</h4>
-      <p>{ props.shoes.price }</p>
+    <h4>{props.fruits.title }</h4>
+    <p>{ props.fruits.price }</p>
     </div>
   )
 }
-  
+
+
+
 export default App;
